9f6db27e65f69d463c2ffc66756bf5e53652b3f7
Colter Nichols <colter.nichols02@gmail.com>
2022-09-16 00:31:26 -0400

    Script for mouse position and element count done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
 
 /* javascript */
 
+const page = document.querySelector("body");
+const position = document.getElementById('position');
+const portrait = document.querySelector("div.portrait");
+const portraitElements = portrait.querySelectorAll("div");
+
+var elementCount = portraitElements.length;
+
+function updatePosition(event) {
+  var x = event.pageX;
+  var y = event.pageY;
+  var coords = "X coords: " + x + ", Y coords: " + y;
+  position.innerText = coords;
+}
+
+page.addEventListener("mousemove", updatePosition, false);
+page.addEventListener("mouseenter", updatePosition, false);
+page.addEventListener("mouseleave", updatePosition, false);
+
+
+
 var bauhaus = `
 
 Staaatliches Bauhaus (1919-1933)
@@ -18,3 +38,4 @@ Staaatliches Bauhaus (1919-1933)
 `;
 
 console.log(bauhaus);
+console.log(elementCount);
